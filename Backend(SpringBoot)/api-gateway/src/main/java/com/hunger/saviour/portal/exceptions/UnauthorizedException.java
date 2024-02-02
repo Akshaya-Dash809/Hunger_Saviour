b84d5c98a086b93b307e8cf61270c5f4b7a62fcb
Akshaya-Dash809 <111556367+Akshaya-Dash809@users.noreply.github.com>
2024-02-02 18:16:58 +0530

    Delete Backend(SpringBoot)/api-gateway directory

--- Backend(SpringBoot)/api-gateway/src/main/java/com/hunger/saviour/portal/exceptions/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.hunger.saviour.portal.exceptions;
-
-public class UnauthorizedException extends RuntimeException{
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
